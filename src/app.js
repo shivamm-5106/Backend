c95800b02a420bc09eccc54b97178e32b4e97a8c
Shivam Pandey <shivampandey5106@gmail.com>
2025-09-24 11:23:18 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -11,8 +11,21 @@ app.use(cors({
 }));
 
 app.use(express.json({limit:"16kb"}));
-app.use(urlencoded({extended:true, limit:"16kb"}));
+app.use(express.urlencoded({extended:true, limit:"16kb"}));
 app.use(express.static("public"));
-app.use(cookieParser);
+app.use(cookieParser());
+
+
+
+// routes
+
+import userRouter from "./routes/user.routes.js";
+
+//route declaration
+
+app.use("/api/v1/users",userRouter);
+
+
+
 
 export default app;
